fe26bf5f06371c8fa5234baaa903350155b12f3e
Talita Alves <67616021+TalitaAlvs@users.noreply.github.com>
2022-12-12 17:48:06 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -23,7 +23,7 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
 
-     thisAmount = amountFor(each);
+     thisAmount = each.getCharge();
 
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
@@ -50,23 +50,4 @@ public class Customer {
              " frequent renter points";
      return result;
    }
-
-   private double amountFor(Rental aRental) {
-      switch (each.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
-             break;
-      }
-   }
-
 }
